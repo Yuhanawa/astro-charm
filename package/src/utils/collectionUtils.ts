e622a83196c808b2b3673a5bccb2f0c9e377f823
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-12-27 21:09:01 +0800

    fix: now post can hidden on prod v1.0.2

--- package/src/utils/collectionUtils.ts
+++ package/src/utils/collectionUtils.ts
@@ -1,10 +1,11 @@
 import { getCollection } from "astro:content";
 
 export async function getPostEntries(options?: { showHidden?: boolean }) {
-  const entries = await getCollection("posts", ({ data }) =>
-    import.meta.env.PROD
-      ? data.draft !== true
-      : true && (!data.hidden || options?.showHidden),
+  const entries = await getCollection(
+    "posts",
+    ({ data }) =>
+      (!data.hidden || options?.showHidden) &&
+      (import.meta.env.PROD ? data.draft !== true : true),
   );
   return entries.sort(
     (a, b) =>
